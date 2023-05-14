d59e7ac2c95e4d918c1506a277e153fc50d52f35
Matías B Crespo <matisfd@gmail.com>
2023-05-14 17:10:47 -0300

    quito formulario y agrego servicio para actores

--- src/app/components/pages/alta-actores/alta-actores.component.ts
+++ src/app/components/pages/alta-actores/alta-actores.component.ts
@@ -1,39 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Actor } from 'src/app/clases/actor/actor';
 import { Pais } from 'src/app/clases/pais/pais';
 import { PaisInterface } from 'src/app/interfaces/pais/pais';
+import { ActorService } from 'src/app/services/actor/actor.service';
 
 @Component({
   selector: 'app-alta-actores',
   templateUrl: './alta-actores.component.html',
-  styleUrls: ['./alta-actores.component.css']
+  styleUrls: ['./alta-actores.component.css'],
 })
 export class AltaActoresComponent {
-
   form!: FormGroup;
   @Input() pais: Pais = new Pais();
+  actorService!: ActorService;
+
+  constructor(actorService: ActorService) {
+    this.actorService = actorService;
+  }
+
+  //#region NG Hooks
 
   ngOnInit(): void {
+    // console.log(this.pais)
     this.form = new FormGroup({
       // usuario : new FormControl('',)
       nombre: new FormControl('', [Validators.pattern('^[a-zA-Z]+$')]),
       apellido: new FormControl('', [Validators.pattern('^[a-zA-Z]+$')]),
-      id: new FormControl('', [Validators.pattern('^[a-zA-Z]+$')]),
+      edad: new FormControl('', [Validators.pattern('^[a-zA-Z]+$')]),
+      // id: new FormControl('', [Validators.pattern('^[a-zA-Z]+$')]),
       flag: new FormControl('', [Validators.pattern('^[a-zA-Z]+$')]),
       nacionalidad: new FormControl('', [Validators.pattern('^[a-zA-Z]+$')]),
     });
   }
 
-  actor : Actor = new Actor();
-  paisDesdeTabla! : PaisInterface;
+  //#endregion
 
-  recibirItemDeHijo(paisRecibido : PaisInterface){
-    this.paisDesdeTabla = paisRecibido;
-    console.log('en el padre')
-    console.log(paisRecibido)
-  }
+  //#region Propiedades
+  actor: Actor = new Actor();
+  paisDesdeTabla!: PaisInterface;
 
+  //#endregion
+
+  //#region Getters
   get nombre() {
     return this.form.get('nombre');
   }
@@ -42,9 +51,13 @@ export class AltaActoresComponent {
     return this.form.get('apellido');
   }
 
-  get id() {
-    return this.form.get('id');
+  get edad() {
+    return this.form.get('edad');
   }
+
+  // get id() {
+  //   return this.form.get('id');
+  // }
 
   get flag() {
     return this.form.get('flag');
@@ -54,7 +67,38 @@ export class AltaActoresComponent {
     return this.form.get('nacionalidad');
   }
 
-  CrearActor(){
-    console.log(this.actor)
+  //#endregion
+
+  //#region Metodos
+  recibirItemDeHijo(paisRecibido: PaisInterface) {
+    this.paisDesdeTabla = paisRecibido;
   }
+
+  CrearActor() {
+    //TODO Me sirve para actualizar el valor del pais seleccionado en este formulario.
+    this.form.patchValue({
+      nacionalidad: this.paisDesdeTabla.name,
+      flag: this.paisDesdeTabla.flag,
+    });
+
+    // id: string = '',
+    // nombre: string = '',
+    // apellido: string = '',
+    // edad: number = 0,
+    // nacionalidad: string = '',
+    // flag: string = ''
+
+    this.actorService.guardar(
+      new Actor(
+        '',
+        this.nombre?.value,
+        this.apellido?.value,
+        this.edad?.value,
+        this.nacionalidad?.value,
+        this.flag?.value
+      )
+    );
+  }
+
+  //#endregion
 }
--- src/app/modulos/actor-alta/actor-alta.module.ts
+++ src/app/modulos/actor-alta/actor-alta.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { ActorAltaRoutingModule } from './actor-alta-routing.module';
 import { ActorAltaComponent } from './actor-alta.component';
 import { AltaActoresComponent } from '../../components/pages/alta-actores/alta-actores.component';
-import { AltaActorComponent } from '../../components/presentacion/actor/alta-actor/alta-actor.component';
 import { TablaPaisesComponent } from '../../components/pages/pais/tabla-paises/tabla-paises.component';
 import { DetallePaisComponent } from '../../components/presentacion/pais/detalle-pais/detalle-pais.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -14,9 +13,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [
     ActorAltaComponent,
     AltaActoresComponent,
-    AltaActorComponent,
     TablaPaisesComponent,
-    DetallePaisComponent,
+    DetallePaisComponent
+    
   ],
   imports: [
     CommonModule,
